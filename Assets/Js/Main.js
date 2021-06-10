be76d9e3c4a0e3979f264fad280620163d7e44bc
DanielQuilcue <danielquilcue18@gmail.com>
2021-06-10 16:58:35 -0500

    Header & Nav y Menu

---
+++ Assets/Js/Main.js
@@ -0,0 +1,30 @@
+        /**
+         * MENU SHOW Y HIDDEN
+        **/
+    
+const navMenu =  document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+
+        /**
+         * MENU SHOW
+        **/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+        /**
+         * MENU HIDDEN
+        **/
+
+/* Validate if constant exist */
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
